1d3a8b87b12653c6bf88ebfa73954feb1f9d0eb3
gauravraj0408 <36809025+gauravraj0408@users.noreply.github.com>
2020-02-12 12:57:05 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -14,7 +14,7 @@ clear
             A Bug is Never just a Mistake 
 mrrobot
 echo " "
-echo -e "\e[92m This script will take 20 - 30 Mins to be installed, Stay Back and Enjoy. "
+echo -e "\e[92m This script will take 20 - 30 Mins to be installed, Stay Back and Enjoy "
 echo " "
 pkg install unstable-repo -y
 pkg install metasploit -y
