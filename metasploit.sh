eb1297470d60dbbaaa1813e1ef89d4405d7c79c0
gauravraj0408 <36809025+gauravraj0408@users.noreply.github.com>
2020-02-12 12:55:15 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -13,6 +13,8 @@ clear
  ╚═════════════════════════════════════════════════╝                                 
             A Bug is Never just a Mistake 
 mrrobot
+echo " "
 echo -e "\e[92m This script will take 20 - 30 Mins to be installed, Stay Back and Enjoy. "
+echo " "
 pkg install unstable-repo -y
 pkg install metasploit -y
